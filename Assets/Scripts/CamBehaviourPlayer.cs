ca73b303f319379a3df0e0a43046c561d8112dde
jeroen <jeroen_kruyt@hotmail.com>
2022-02-22 16:41:03 +0100

    Camera for player
    cam movement with boundaries max and min location it can go in both the x and y axis

--- Assets/Scripts/CamBehaviourPlayer.cs
+++ Assets/Scripts/CamBehaviourPlayer.cs
@@ -1,21 +1,26 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 namespace Scripts
 {
     public class CamBehaviourPlayer : MonoBehaviour
     {
-        // Start is called before the first frame update
-        void Start()
-        {
-        
-        }
+        [Header("Components:")] public Transform target;
 
-        // Update is called once per frame
-        void Update()
+        [Header("Smoothing of cam:")] public float smoothing;
+
+        [Header("Max and Min range for cam to go:")]
+        public Vector2 maxPos;
+
+        public Vector2 minPos;
+
+        private void FixedUpdate()
         {
-        
+            var targetPos = new Vector3(target.position.x, target.position.y, transform.position.z);
+
+            targetPos.x = Mathf.Clamp(targetPos.x, minPos.x, maxPos.x);
+            targetPos.y = Mathf.Clamp(targetPos.y, minPos.y, maxPos.y);
+
+            transform.position = Vector3.Lerp(transform.position, targetPos, smoothing);
         }
     }
 }
